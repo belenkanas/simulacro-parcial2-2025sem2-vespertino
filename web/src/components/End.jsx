ad144ec781c09e9ed05f64c236b07aa5ad6009ac
belenkanas <maria.kanas@correo.ucu.edu.uy>
2025-10-18 18:48:00 -0300

    elementos en End

--- web/src/components/End.jsx
+++ web/src/components/End.jsx
@@ -5,12 +5,17 @@ import VisitadosContext from './contexts/Vistados'
 
 export default function End() {
     
+    const navigate = useNavigate();
     const {mode} = useContext(VisitadosContext);
     
-    async function obtenerPaises() {
+    async function obtenerVisitados() {
         try{
             const response = await fetch('/api/countries')
             if (!response.ok) throw new Error('Error al obtener países')
+            for (codigos of mode){
+                //Ha
+            }
+        
         }
         catch (error){
             console.error('Error: ', error.message)
@@ -18,13 +23,32 @@ export default function End() {
     }
 
     useEffect(()=> {
-        obtenerPaises;
+        obtenerVisitados;
     }, []);
 
     return (
     <>
         <h1>Fin del juego</h1>
         <h2>Usted visitó los siguientes países</h2>
+
+
+        <div className='banderasBotones'>
+                {paises.map((pais, i) => (
+                    <button 
+                    key={i}
+                    onClick={() => handleCorrecto(pais)}>
+                        <img
+                        src={pais.flag.svg}
+                        alt={pais.flag.alt ?? `Bandera de ${pais.name?.common}`}
+                        width="100"
+                        />
+                    </button>
+                ))}
+        </div>
+
+        <button 
+            onClick={()=> navigate('./')}
+        ></button>
     </>
   )
 }
